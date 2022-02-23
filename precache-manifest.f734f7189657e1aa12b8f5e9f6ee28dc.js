d7cb2178b10585874e22cb47ff886e42292defa4
elleVas <elleVas@users.noreply.github.com>
2022-02-23 17:20:53 +0000

    deploy: 28905f6315731628715e776a1c14d1a0b7b146e9

---
+++ precache-manifest.f734f7189657e1aa12b8f5e9f6ee28dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a475fb894e63ccd33f8e56d310ee722d",
+    "url": "https://elleVas.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "94acced7faff6f5ced86",
+    "url": "https://elleVas.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "3daea9c8b86321f7fc3b",
+    "url": "https://elleVas.github.io/task-management-frontend/static/js/2.36db7500.chunk.js"
+  },
+  {
+    "revision": "94acced7faff6f5ced86",
+    "url": "https://elleVas.github.io/task-management-frontend/static/js/main.4340ad44.chunk.js"
+  },
+  {
+    "revision": "4d04887ad6e646921385",
+    "url": "https://elleVas.github.io/task-management-frontend/static/js/runtime~main.0d421080.js"
+  }
+]);
